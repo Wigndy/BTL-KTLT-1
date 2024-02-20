879cf93c311d436255c2b4d296ed6c6fca165b81
Diep_Kim_Phong <159527565+Wigndy@users.noreply.github.com>
2024-02-20 22:31:44 +0700

    Update study_in_pink1.cpp

--- study_in_pink1.cpp
+++ study_in_pink1.cpp
@@ -35,10 +35,84 @@ bool readFile(
 ////////////////////////////////////////////////////////////////////////
 
 // Task 1
-int firstMeet(int & exp1, int & exp2, int e1) {
-    // TODO: Complete this function
-    
+// HPH1 và HPH2 khoảng từ từ0-666 out khoảng trên mã hoặc dưỡi min thiết lập theo
+// exp exp1 và và2 từ từ0 đến đến600
+// M M1 và và2 0 3000
+// E mã sự kiên kiên 0 99
+int case1(int & exp1, int & exp2, int e1)
+{
+    switch (e1)
+    {
+    case 0:
+        exp2 += 29;
+        break;
+    case 1:
+        exp2 += 45;
+        break;
+    case 2:
+        exp2 += 75;
+        break;
+    case 3:
+        exp2 = exp2 + 29 + 45 + 75;
+        break;
+    }
+    int D = e1 * 3 + exp1 * 7;
+    if (D & 1 == 0)
+        exp1 = exp1 + D/200;
+    else
+        exp1 = exp1 - D/100;
     return exp1 + exp2;
+}//nontested
+int subcase2_1(int e1)
+{
+    return e1/4 + 19;
+}
+int subcase2_2(int e1)
+{
+    return e1/9 + 21;
+}
+int subcase2_3(int e1)
+{
+    return e1/16 + 17;
+}
+int case2(int &exp1, int & exp2, int e1)
+{
+    if (e1 > 3 && e1 < 20)
+        exp2 += subcase2_1(e1);
+
+    if (e1 >= 20 && e1 < 50)
+        exp2 += subcase2_2(e1);
+
+    if (e1 >= 50 && e1 <= 65)
+    {
+        exp2 += subcase2_3(e1);
+        exp1 -= e1;
+    }
+    if (e1 >=66 && e1 <= 79)
+    {
+        exp2 += subcase2_1(e1);
+        if (exp2 > 200)
+            exp2 += subcase2_2(e1);
+    }
+    if (e1 >=80 && e1 <= 99)
+    {
+        exp2 += subcase2_1(e1);
+        exp2 += subcase2_2(e1);
+        if (exp2 > 400)
+        {
+            exp2 += subcase2_3(e1);
+            exp2 *= 1.15;
+            exp1 -= e1;
+        }
+            
+    }
+}//nontested
+int firstMeet(int & exp1, int & exp2, int e1) {
+    if (e1 >=0 && e1 <= 3)
+        return case1(exp1, exp2, e1);
+    else
+        return case2(exp1, exp2, e1);
+    
 }
 
 // Task 2
